40084f50e0b45e102d344abcedb487e581438f55
ymulenll <ymulenll@gmail.com>
2022-02-07 16:57:23 -0300

    update title

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export function App() {
     <Router>
       <header>
         <Link to="/">
-          <h1 className={styles.title}>Movies</h1>
+          <h1 className={styles.title}>🍿🍿 Movies 🍿🍿</h1>
         </Link>
       </header>
       <main>
